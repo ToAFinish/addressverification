faa6f14087923f278a141959c59b56168f6c9cb3
Joseph Dindinger <signup@toafinish.com>
2019-01-25 10:29:38 -0600

    New

---
+++ flows/No_Saving/VerifyAnAddressController.js
@@ -0,0 +1,33 @@
+({
+	init : function(cmp, event, helper) {
+		 // Figure out which buttons to display
+        var availableActions = cmp.get('v.availableActions');
+        for (var i = 0; i < availableActions.length; i++) {
+            if (availableActions[i] == "PAUSE") {
+                cmp.set("v.canPause", true);
+            } else if (availableActions[i] == "BACK") {
+                cmp.set("v.canBack", true);
+            } else if (availableActions[i] == "NEXT") {
+                cmp.set("v.canNext", true);
+            } else if (availableActions[i] == "FINISH") {
+                cmp.set("v.canFinish", true);
+            }
+        }
+	},  
+    onButtonPressed: function(cmp, event, helper) {
+        // Figure out which action was called
+        var actionClicked = event.getSource().getLocalId();
+        if(actionClicked=='FINISH'){
+            cmp.set("v.DoneProcess","true");
+        }else{
+            // Call that action
+            var navigate = cmp.getEvent("navigateFlowEvent");
+            navigate.setParam("action",actionClicked);
+            navigate.fire();
+        }
+    },
+    handleVerify: function(component, event, helper){
+        component.set('v.doingProcess','true');       
+        helper.getpostal(component,event,helper);               
+    }
+})
