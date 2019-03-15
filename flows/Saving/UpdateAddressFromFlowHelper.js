81619c9ea214cc14d9d491228717cab7165650a2
Joseph Dindinger <signup@toafinish.com>
2019-03-15 13:56:33 -0500

    optional feature

--- flows/Saving/UpdateAddressFromFlowHelper.js
+++ flows/Saving/UpdateAddressFromFlowHelper.js
@@ -37,87 +37,101 @@
         }
     },
     getpostal : function(component, event, helper) {
-      
+        
         var isblank='false';
 		var index=component.get("v.verifiedindex");
         var city=document.getElementById('city'+index).value;
         var state=document.getElementById('state'+index).value;
         var street=document.getElementById('s'+index).value;
-		var country=document.getElementById('country'+index).value;               
-       
-        var street2='';
-        if(document.getElementById('s2'+index)!=null){
-           street2 =document.getElementById('s2'+index).value;
-        }
+		var country=document.getElementById('country'+index).value;     
+        var zip=document.getElementById('zip'+index).value;
+        console.log('index in get postal'+index);
+        var isverificationNeeded=document.getElementById('need'+index).checked;
         
-        var zip='';
-        if(city!='' || state!='' || street!='')
-            isblank='false';
-        else
-            isblank='true';
-        
-        var el=document.getElementById('s'+index);  
-        var _x = 0;
-        var _y = 0;
-      
-        try{
-            while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
-                _x += el.offsetLeft - el.scrollLeft;
-                _y += el.offsetTop - el.scrollTop;
-                el = el.offsetParent;
-            }	
-        }catch(e){}
-        
-    	{            
-            var action = component.get("c.getpostalcode");
-            action.setParams({
-                "street": street,
-                "city": city,
-                "state": state,
-                "country": country,
-                "zip": zip,
-                "street2":street2
-            });
+        if(isverificationNeeded==false){
             
-            action.setCallback(this, function(response) {            
-                if (response.getState() == "SUCCESS") {            
-                    document.getElementById('zip'+index).value=response.getReturnValue(); 
-                    if(response.getReturnValue()!=''){                             
-                        component.set("v.verifiedindex",parseInt(index)+1);                                            
-                        //  NOW MAKE THE REVERIY RUN
-                        document.getElementById('verifyagain').click();
-                    }
-                    else{
-                        // OPEN THE ALERT BOX   
-                                                              
-                        document.getElementById("ErrorboxMain").style.display='';                        
-                        document.getElementById("Errorbox").style.paddingTop=(_y-20)+'px';   
-                        
-                     //   document.getElementById("Errorbox").style.marginTop=10+(parseInt(index)*15);
-                        
-                        if((parseInt(index)+1)%2==0){
-                            document.getElementById("Errorbox").style.cssFloat='Right';
-                            document.getElementById("Errorbox").style.marginLeft='0px';
-                            document.getElementById("Errorbox").style.marginRight='33px';
-                        }else{
-                            document.getElementById("Errorbox").style.cssFloat='Left';
-                            document.getElementById("Errorbox").style.marginLeft='33px';
-                            document.getElementById("Errorbox").style.marginRight='0px';
+            var street2='';
+            if(document.getElementById('s2'+index)!=null){
+                street2 =document.getElementById('s2'+index).value;
+            }
+                      
+            if(city!='' || state!='' || street!='')
+                isblank='false';
+            else
+                isblank='true';
+            
+            var el=document.getElementById('s'+index);  
+            var _x = 0;
+            var _y = 0;
+            
+            try{
+                while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+                    _x += el.offsetLeft - el.scrollLeft;
+                    _y += el.offsetTop - el.scrollTop;
+                    el = el.offsetParent;
+                }	
+            }catch(e){}
+            
+            {            
+                var action = component.get("c.getpostalcode");
+                action.setParams({
+                    "street": street,
+                    "city": city,
+                    "state": state,
+                    "country": country,
+                    "zip": zip,
+                    "street2":street2
+                });
+                
+                action.setCallback(this, function(response) {            
+                    if (response.getState() == "SUCCESS") {            
+                        document.getElementById('zip'+index).value=response.getReturnValue(); 
+                        if(response.getReturnValue()!=''){                             
+                            component.set("v.verifiedindex",parseInt(index)+1);                                            
+                            //  NOW MAKE THE REVERIY RUN
+                            document.getElementById('verifyagain').click();
                         }
-                        
-                        document.getElementById("theMessage").innerHTML=document.getElementById('s'+index).value+' '+street2+' '+document.getElementById('city'+index).value+' '+document.getElementById('state'+index).value;
-                        if(isblank=='false'){
-                            document.getElementById("ErrorboxHeader").innerHTML='YOU ENTERED AN UNKNOWN ADDRESS';
-                        }else{
-                            document.getElementById("ErrorboxHeader").innerHTML='YOU DID NOT ENTER ENOUGH INFORMATION';
-                        }                                                        
+                        else{
+                            // OPEN THE ALERT BOX   
+                            
+                            document.getElementById("ErrorboxMain").style.display='';                        
+                            document.getElementById("Errorbox").style.paddingTop=(_y-20)+'px';   
+                            
+                            //   document.getElementById("Errorbox").style.marginTop=10+(parseInt(index)*15);
+                            
+                            if((parseInt(index)+1)%2==0){
+                                document.getElementById("Errorbox").style.cssFloat='Right';
+                                document.getElementById("Errorbox").style.marginLeft='0px';
+                                document.getElementById("Errorbox").style.marginRight='33px';
+                            }else{
+                                document.getElementById("Errorbox").style.cssFloat='Left';
+                                document.getElementById("Errorbox").style.marginLeft='33px';
+                                document.getElementById("Errorbox").style.marginRight='0px';
+                            }
+                            
+                            document.getElementById("theMessage").innerHTML=document.getElementById('s'+index).value+' '+street2+' '+document.getElementById('city'+index).value+' '+document.getElementById('state'+index).value;
+                            if(isblank=='false'){
+                                document.getElementById("ErrorboxHeader").innerHTML='YOU ENTERED AN UNKNOWN ADDRESS';
+                            }else{
+                                document.getElementById("ErrorboxHeader").innerHTML='YOU DID NOT ENTER ENOUGH INFORMATION';
+                            }                                                        
+                        }
                     }
-                }
-            });
-            $A.enqueueAction(action);       
-        } 
-	},
-    saveAddressAfterSucess : function(component, event, helper) {        
+                });
+                $A.enqueueAction(action);       
+            } 
+        }else{
+           
+            var verifiedIndex=component.get('v.verifiedindex');
+            verifiedIndex=parseInt(verifiedIndex)+1;
+        	component.set('v.verifiedindex',verifiedIndex);                                        
+            //  NOW MAKE THE REVERIY RUN          
+            //  document.getElementById('verifyagain').click();
+            $A.enqueueAction(component.get('c.reverify'));
+        }
+    },
+    saveAddressAfterSucess : function(component, event, helper) { 
+       
         var addresses=component.get('v.sObjaddresses');
         for(var i=0;i<addresses.length;i++){           
             var oneRow=addresses[i].oneRow; 
@@ -128,7 +142,9 @@
                 addresses[i].oneRow[j].streetvalue=document.getElementById('s'+index).value;             
                 addresses[i].oneRow[j].countryvalue=document.getElementById('country'+index).value;
                 addresses[i].oneRow[j].postalcodevalue=document.getElementById('zip'+index).value;
-             
+                
+                addresses[i].oneRow[j].IsSelectedToVerify=document.getElementById('need'+index).checked;
+                
                 //IF STREET 2 IS ENABLED
                 if(addresses[i].oneRow[j].street2Enabled){
                     addresses[i].oneRow[j].street2value=document.getElementById('s2'+index).value;
