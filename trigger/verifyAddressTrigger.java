b7ec6066baf2dca12c36a696189f6b51aeaf1715
Joseph Dindinger <signup@toafinish.com>
2021-05-03 14:47:54 -0500

    Update and rename verifyAddressTrigger.java to verifyTrigger.java

--- trigger/verifyAddressTrigger.java
+++
@@ -1,7 +0,0 @@
-/*
-    THIS TRIGGER AUTOMATES THE VERIFICATION OF AN ADDRESS
-    ON A CUSTOM OBJECT (OFFICE__C)
-*/
-trigger autoVerifyOfficeAddress on Office__c (before insert, before update, after insert, after update) {
-    new smartystreets.AutoVerificationServices().HandleAutoVerify('Office__c');
-}
---
+++ trigger/verifyTrigger.java
@@ -0,0 +1,10 @@
+/*
+    THIS TRIGGER AUTOMATES THE VERIFICATION OF AN ADDRESS, EMAIL OR PHONE
+    ON A CUSTOM OBJECT (OFFICE__C)
+    COMMENT OUT THE VERIFICATIONS YOU DO NOT NEED
+*/
+trigger verifyTrigger on Office__c (before insert, before update, after insert, after update) {
+    new smartystreets.AutoVerificationServices().HandleAutoVerify('Office__c'); // for PHYSICAL ADDRESSES
+    new smartystreets.AutoVerificationServicesEmails().HandleAutoVerify('Office__c'); // for EMAIL ADDRESSES
+    new smartystreets.AutoVerificationServicesPhone().HandleAutoVerify('Office__c'); // for PHONE NUMBERS
+}
