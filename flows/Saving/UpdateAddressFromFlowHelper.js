e384ba2869d892fcfe560376fdb1be3b5516839a
Joseph Dindinger <signup@toafinish.com>
2022-11-03 13:57:48 -0500

    Update for Autocomplete Pro

--- flows/Saving/UpdateAddressFromFlowHelper.js
+++ flows/Saving/UpdateAddressFromFlowHelper.js
@@ -1,40 +1,85 @@
 ({	
-    addSuggestions:function (component, event, helper){
+    addSuggestions:function (component, event, helper,searchInfo){
+        console.log('*** in helper');
+        //THIS IS TO SHOW SUGGESTION WHEN USER TYPE IN ADDRESS STREET FIELD
+        //THIS CALLS API SUGGEST OF SMARTYSTREETS
         
-        var currentIndex=parseInt((event.target.id).replace('s',''));
-        var totaladdress=component.get("v.TotalAddress");
-        for(var i=0;i<totaladdress;i++){
-            if(i!=currentIndex)  {
-                document.getElementById('street'+i).style.display='none';
-            }  
-        }
-         
-    	var addValue=event.target.value;
-       
-        if(addValue!=''){
-        var action = component.get("c.getaddresses");
-        action.setParams({
-            "prefix": addValue
-        });
-        var opts = [];
-        action.setCallback(this, function(response) {
-            if (response.getState() == "SUCCESS") {
-                var allValues = response.getReturnValue();
-                component.set('v.addresses',response.getReturnValue());
-                if (allValues != undefined && allValues.length>0) {
-                    component.set('v.index',(event.target.id).replace('s',''));
-                    document.getElementById('street'+(event.target.id).replace('s','')).style.display='';
-                }else{
-                    document.getElementById('street'+(event.target.id).replace('s','')).style.display='none';
-                }                             
+        component.set('v.index',(event.target.id).replace('s',''));
+        var index = component.get('v.index');
+        console.log('INDEX'+index);
+        var country= document.getElementById('country'+index).value;
+        console.log('country helper'+country);
+        
+        if(country==undefined || country=='' ||
+           (country!=undefined && (country.toLowerCase()=='usa') 
+		   || country.toLowerCase().trim()=='united states'
+           || country.toLowerCase().trim()=='united states of america'
+           || country.toLowerCase().trim()=='us' )
+        ){
+            var addValue=event.target.value;   
+            var param =addValue+';'+'US'; 
+            console.log('***** param='+param); 
+            console.log('****** '+addValue); 
+            if(addValue!='' && addValue.length>3){ 
+                console.log('*** call server');
+               //GET THE SUGGESTIONS
+                var action = component.get("c.getDropdownAddresses");
+                action.setParams({
+                    "Parameters": param
+                });
+                var opts = [];
+                action.setCallback(this, function(response) {
+                    if (response.getState() == "SUCCESS") {
+                        console.log('******* THE API CALLED');
+                        console.log(JSON.stringify(response.getReturnValue()));
+                        var allValues = response.getReturnValue();
+                        console.log('allValues '+allValues);
+                        component.set('v.addresses',response.getReturnValue());
+                        
+                        //IF THERE ARE MORE THAN ONE ADDRESS THEN SHOW DROPDOWN
+                        if (allValues != undefined && allValues.length>0){
+                            console.log('allValues length '+allValues.length);
+                            var dropdownDiv=document.getElementById('streetDropdown');
+                            var streetDropdownPanel=document.getElementById('streetDropdownPanel');
+                            if(index==0){
+                                console.log('inside 0');
+                                streetDropdownPanel.classList.remove("dropDownCss1");
+                                streetDropdownPanel.classList.add("dropDownCss0");
+                            }
+                            if(index==1){
+                                console.log('inside 1');
+                                streetDropdownPanel.classList.remove("dropDownCss0");
+                                streetDropdownPanel.classList.add("dropDownCss1");
+                            }
+                            dropdownDiv.classList.remove("achide");
+                            dropdownDiv.classList.add("acshow");
+                        } 
+                        //HIDE THE DROPDOWN
+                        else{
+                            var dropdownDiv=document.getElementById('streetDropdown');
+                            dropdownDiv.classList.remove("acshow");
+                            dropdownDiv.classList.add("achide");
+                        }   
+                        searchInfo.timeId=undefined;
+                        component.set("v.searchInfo",searchInfo);                 
+                    }
+                });
+                $A.enqueueAction(action);
             }
-        });
-        $A.enqueueAction(action);
-        }else{
-            document.getElementById('street'+(event.target.id).replace('s','')).style.display='none';
-            var allValues=[];
-        	component.set('v.addresses',allValues);     
-        }
+            //IF STREET IS BLANK THEN RESET THE DROPDOWN 
+            else{
+                console.log('*** search key not full');
+                //HIDE THE DROPDOWN VALUES
+                var dropdownDiv=document.getElementById('streetDropdown');
+                dropdownDiv.classList.remove("acshow");
+                dropdownDiv.classList.add("achide");
+                //CLEAR THE DROPDOWN VALUES
+                var allValues=[];
+                component.set('v.addresses',allValues); 
+                searchInfo.timeId=undefined;
+                component.set("v.searchInfo",searchInfo);
+            }
+        }        
     },
     getpostal : function(component, event, helper) {
         
@@ -166,5 +211,16 @@
             }
         });
         $A.enqueueAction(action);
+    },
+    //HIDE THE SUGGESTIONS IF HOVER IS REMOVED 
+    hideAutoSuggestion : function(component) {
+        console.log('hideAutoSuggestion');
+        var index = component.get('v.index');
+        if(document.getElementById('s'+index)!=null){
+            console.log('hover removed');
+            var dropdownDiv=document.getElementById('streetDropdown');
+            dropdownDiv.classList.remove("acshow");
+            dropdownDiv.classList.add("achide");
+        }
     }
 })
