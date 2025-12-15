5d398bbede6d1d6c97b6b66c55f2a03e7f5af9fc
Joseph Dindinger <signup@toafinish.com>
2025-12-14 21:49:14 -0600

    Add files via upload

---
+++ flows/saving2/GetAddressFromFlowController.js
@@ -0,0 +1,289 @@
+({
+    handleNavigate: function(cmp, event) {      
+        if(event.getParam("action")=='FINISH'){
+            cmp.set("v.DoneProcess","true");
+        }else{
+        var navigate = cmp.get("v.navigateFlow");
+        navigate(event.getParam("action"));    
+        }
+    },
+    doInit : function(component, event, helper) {      
+      //  helper.objectName(component);      
+        var action = component.get("c.getAllAddrForObjectName");
+        action.setParams({ objectName : component.get("v.objectName") });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {                            
+                component.set("v.sObjaddresses", response.getReturnValue()); 
+                console.log(JSON.stringify(response.getReturnValue()));
+                if(response.getReturnValue().length==0){
+                     component.set('v.Error2','True');
+                }else{
+                    var addresses=component.get('v.sObjaddresses');
+                    var noOfaddress=0;                  
+                    for(var i=0;i<addresses.length;i++){
+                        var oneRow=addresses[i].oneRow; 
+                        noOfaddress+=oneRow.length;
+                    }
+                    component.set('v.TotalAddress',noOfaddress);
+                }
+            }
+            else if (state === "INCOMPLETE") {
+                // do something
+            }
+            else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " +
+                                 errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
+        });
+        $A.enqueueAction(action);
+	},
+    
+    closeselection: function(component, event, helper) {
+        component.set("v.autocompleteEntries",false);
+        component.set("v.search",undefined);
+        component.set("v.searchtext",undefined);
+        var index=component.get('v.index'); 
+        var dropdownDiv=document.getElementById('streetDropdown');
+        dropdownDiv.classList.remove("acshow");
+        dropdownDiv.classList.add("achide");
+    },
+    
+    getselected : function(component, event, helper) {
+        let addresse=event.getParam('theAddress');
+        console.log(JSON.stringify(addresse));
+        component.set("v.autocompleteEntries",false);
+        component.set("v.search",undefined);
+        component.set("v.searchtext",undefined);
+        var index=component.get('v.index');    
+        //SET THE FIELD VALUES FROM THE SELECTED OPTION    
+        document.getElementById('city'+index).value=addresse.city;
+        document.getElementById('state'+index).value=addresse.state;
+        document.getElementById('s'+index).value=addresse.street_line;
+        document.getElementById('zip'+index).value=addresse.zipcode;
+        
+        //HIDE THE DROPDOWN OPTION AFTER SELECTED
+        //document.getElementById('street'+index).style.display='none';
+        var dropdownDiv=document.getElementById('streetDropdown');
+        dropdownDiv.classList.remove("acshow");
+        dropdownDiv.classList.add("achide");
+        component.set("v.stopSubmit",false);
+        
+        //CLEAR THE ADDRESS OPTINS 
+        var allValues=[];
+        component.set('v.addresses',allValues);  
+        
+        //CHECK IF DO NOT VERIFY IS ENABLED,THE PAID AUTOCOMPLETE IS USED
+        var doNotVerify=component.get("v.doNotVerify");       
+        if(doNotVerify==true){
+            //GET ALL THE ADDRESSES
+            var allAddresses=component.get('v.sObjaddresses');
+            for(var l=0;l<allAddresses.length;l++){
+                var oneRow=allAddresses[l].oneRow;
+                for(var j=0;j<oneRow.length;j++){
+                    //IF CURRENT ADDRESS INDEX IS MATCH WITH ADDRESS OBJECT
+                    if(oneRow[j].Index==index){
+                        //UPDATE THE VARIABLE SO THAT NO NEED TO VERIFY OR CHECK LATER
+                        oneRow[j].updateVerified=true;
+                    }
+                }
+            }
+            console.log('******** PAID SUGGESTION SELECTED ');
+            console.log(JSON.stringify(allAddresses));
+        }
+	},
+    
+    callSuggestion: function(component, event, helper){
+        console.log('in call suggestion');
+        //GET ALL THE ADDRESSES
+        var addresses=component.get('v.addresses');
+        
+        var index = component.get('v.index');
+        var country= document.getElementById('country'+index).value;
+        console.log('country JS '+country);        	
+        console.log('INDEX'+index);
+        
+        console.log('addresses[event.target.id].isSelect '+addresses[event.target.id].isSelect);
+        console.log('addresses[event.target.id].search '+addresses[event.target.id].search);
+        console.log('addresses[event.target.id].selected '+addresses[event.target.id].selected);
+        
+        //CHECK IF ADDRESS HAVE ENTRIES TO SELECT
+        if(addresses[event.target.id].isSelect){
+            //SHOW MORE OPTION
+            component.set("v.autocompleteEntries",true);
+            component.set("v.search",addresses[event.target.id].search);
+            component.set("v.searchtext",addresses[event.target.id].selected);
+            return;
+        }
+        
+        //SET THE FIELD VALUES FROM THE SELECTED OPTION   
+        if(country==undefined || country=='' || (country!=undefined && (country.toLowerCase()=='usa') || country.toLowerCase().trim()=='united states' 
+           || country.toLowerCase().trim()=='united states of america' || country.toLowerCase().trim()=='us'))
+        {   
+            console.log('street '+addresses[event.target.id].street_line);
+            console.log('city '+addresses[event.target.id].city);
+            console.log('state '+addresses[event.target.id].state);
+            console.log('zipcode '+addresses[event.target.id].zipcode);
+            document.getElementById('s'+index).value=addresses[event.target.id].street_line;
+            document.getElementById('city'+index).value=addresses[event.target.id].city;
+            document.getElementById('state'+index).value=addresses[event.target.id].state;
+            document.getElementById('zip'+index).value=addresses[event.target.id].zipcode;
+            //document.getElementById('country'+index).value='USA';
+        }else{
+            console.log('inside ELSE');
+            document.getElementById('s'+index).value=addresses[event.target.id].street;	
+            document.getElementById('city'+index).value=addresses[event.target.id].locality;
+            document.getElementById('state'+index).value=addresses[event.target.id].administrative_area;
+            document.getElementById('zip'+index).value=addresses[event.target.id].postal_code;
+        }
+        
+        //HIDE THE DROPDOWN OPTION AFTER SELECTED
+        var dropdownDiv=document.getElementById('streetDropdown');
+        dropdownDiv.classList.remove("acshow");
+        dropdownDiv.classList.add("achide");
+        
+        //CLEAR THE ADDRESS OPTINS 
+        var allValues=[];
+        component.set('v.addresses',allValues);
+    },
+    
+    address: function(component, event, helper){
+        console.log('**** inside Address JS');
+        
+        if (event.keyCode === 13) {
+            event.preventDefault(); 
+            var liIndex=component.get("v.liIndex");
+            if(liIndex!=undefined &&  document.getElementById(liIndex)!=undefined){
+                document.getElementById(liIndex).click();
+            }
+        }else{
+            var searchInfo =component.get('v.searchInfo');
+            console.log(JSON.stringify(searchInfo));
+            if(searchInfo!=undefined){
+                var timeId=searchInfo.timeId;
+                var text=searchInfo.text;
+                var addValue=event.target.value; 
+
+                if(addValue==text || (addValue!=undefined && text!=undefined && addValue.trim()==text.trim() )){
+                    //DO NOTHING
+                    console.log('***** same do nothing');
+                }else{
+                    component.set("v.keydownIndex",-1);
+                    console.log('****** get addresses')
+                    if(timeId!=undefined){
+                        console.log('*** if last one is not finished then stop and queue another');
+                        clearTimeout(timeId);
+                    }
+                    timeId= window.setTimeout(
+                        $A.getCallback(function() {
+                            helper.addSuggestions(component, event, helper,searchInfo);
+                        }), 300
+                    );
+                    searchInfo.timeId=timeId;
+                    searchInfo.text=addValue;
+                    component.set("v.searchInfo",searchInfo);
+                }
+            }else{
+                component.set("v.keydownIndex",-1);
+                searchInfo={};
+                searchInfo.text=event.target.value;
+                timeId= window.setTimeout(
+                    $A.getCallback(function() {
+                        helper.addSuggestions(component, event, helper,searchInfo);
+                    }), 300
+                );
+
+                searchInfo.timeId=timeId;
+                component.set("v.searchInfo",searchInfo);
+            }
+        }
+    },
+    
+    handleVerify: function(component, event, helper){
+        
+        component.set('v.doingProcess','true');
+        var verifiedIndex=component.get('v.verifiedindex');
+        
+        if(verifiedIndex==null || verifiedIndex==''){
+            component.set('v.verifiedindex','0');
+            verifiedIndex='0';
+        }   
+        var noOfaddress=component.get('v.TotalAddress');
+        // THIS METHOD WILL RUN FOR ONCE AND THEN WILL BE STOPPED 
+        
+        if(verifiedIndex<noOfaddress){  
+            helper.getpostal(component,event,helper);         
+        }else{
+            // THIS WILL NOT RUN EVER
+            console.log('before else ran');
+            $A.enqueueAction(component.get('c.reverify')); 
+        }      
+    },
+    reverify:function(component,event,helper){          
+         component.set('v.doingProcess','true');
+        // THIS WILL RUN FOR SECOND ADDERSS OR REVERIY THE ADDRESS             
+        var index= component.get("v.verifiedindex");
+        var noOfaddress=component.get('v.TotalAddress');
+        var close='true';       
+       
+        if(index<noOfaddress){             
+            helper.getpostal(component,event,helper); 
+            close='false';
+        }   
+        if((parseInt(index)==noOfaddress-1 || parseInt(index)==noOfaddress) && close=='true'){
+            
+            document.getElementById("ErrorboxMain").style.display='none';
+            helper.saveAddressAfterSucess(component,event,helper);
+        } 
+	},	
+    goback:function(component,event,helper){ 
+         component.set('v.doingProcess','false');
+         document.getElementById("ErrorboxMain").style.display='none';
+    },
+    asitis:function(component,event,helper){ 
+        component.set('v.doingProcess','false');
+        // THIS WILL CHECK AND RESUBMIT WITH REVERIFY METHOD      
+        var verifiedIndex=component.get('v.verifiedindex');
+        var noOfaddress=component.get('v.TotalAddress');
+        verifiedIndex=parseInt(verifiedIndex)+1;
+        component.set('v.verifiedindex',verifiedIndex);
+        
+        if(verifiedIndex<noOfaddress){
+            helper.getpostal(component,event,helper);
+        }else{                     
+            document.getElementById('verifyagain').click();
+        }
+    },
+    // finalVerify:function(component,event,helper){   
+      
+    //     var action = component.get("c.getverified");
+    //     action.setParams({ recordid : component.get("v.recordId") });
+    //     action.setCallback(this, function(response) {
+    //         var state = response.getState();
+    //         if (state === "SUCCESS") {                            
+    //            component.set('v.readyToMove','true');
+    //            component.set('v.doingProcess','false');
+    //         }          
+    //         else if (state === "ERROR") {
+    //           component.set('v.doingProcess','false');
+    //         }
+    //     });
+    //     $A.enqueueAction(action);
+    // },
+    //HIDE THE SUGGESTIONS IF HOVER IS REMOVED 
+    hideSuggestion: function(component, event, helper){
+        console.log('hideSuggestion');
+        window.setTimeout(
+            $A.getCallback(function() {
+                helper.hideAutoSuggestion(component);
+            }), 200
+        );
+    }
+})
---
+++ flows/saving2/GetAddressFromFlowHelper.js
@@ -0,0 +1,227 @@
+({	
+    addSuggestions:function (component, event, helper,searchInfo){
+        console.log('*** in helper');
+        //THIS IS TO SHOW SUGGESTION WHEN USER TYPE IN ADDRESS STREET FIELD
+        //THIS CALLS API SUGGEST OF SMARTYSTREETS
+        
+        component.set('v.index',(event.target.id).replace('s',''));
+        var index = component.get('v.index');
+        console.log('INDEX'+index);
+        var country= document.getElementById('country'+index).value;
+        console.log('country helper'+country);
+        
+        if(country==undefined || country=='' ||
+           (country!=undefined && (country.toLowerCase()=='usa') 
+		   || country.toLowerCase().trim()=='united states'
+           || country.toLowerCase().trim()=='united states of america'
+           || country.toLowerCase().trim()=='us' )
+        ){
+            var addValue=event.target.value;   
+            var param =addValue+';'+'US'; 
+            console.log('***** param='+param); 
+            console.log('****** '+addValue); 
+            if(addValue!='' && addValue.length>3){ 
+                console.log('*** call server');
+               //GET THE SUGGESTIONS
+                var action = component.get("c.getDropdownAddresses");
+                action.setParams({
+                    "Parameters": param
+                });
+                var opts = [];
+                action.setCallback(this, function(response) {
+                    if (response.getState() == "SUCCESS") {
+                        console.log('******* THE API CALLED');
+                        console.log(JSON.stringify(response.getReturnValue()));
+                        var allValues = response.getReturnValue();
+                        console.log('allValues '+allValues);
+                        component.set('v.addresses',response.getReturnValue());
+                        
+                        //IF THERE ARE MORE THAN ONE ADDRESS THEN SHOW DROPDOWN
+                        if (allValues != undefined && allValues.length>0){
+                            console.log('allValues length '+allValues.length);
+                            var dropdownDiv=document.getElementById('streetDropdown');
+                            var streetDropdownPanel=document.getElementById('streetDropdownPanel');
+                            if(index==0){
+                                console.log('inside 0');
+                                streetDropdownPanel.classList.remove("dropDownCss1");
+                                streetDropdownPanel.classList.add("dropDownCss0");
+                            }
+                            if(index==1){
+                                console.log('inside 1');
+                                streetDropdownPanel.classList.remove("dropDownCss0");
+                                streetDropdownPanel.classList.add("dropDownCss1");
+                            }
+                            dropdownDiv.classList.remove("achide");
+                            dropdownDiv.classList.add("acshow");
+                        } 
+                        //HIDE THE DROPDOWN
+                        else{
+                            var dropdownDiv=document.getElementById('streetDropdown');
+                            dropdownDiv.classList.remove("acshow");
+                            dropdownDiv.classList.add("achide");
+                        }   
+                        searchInfo.timeId=undefined;
+                        component.set("v.searchInfo",searchInfo);                 
+                    }
+                });
+                $A.enqueueAction(action);
+            }
+            //IF STREET IS BLANK THEN RESET THE DROPDOWN 
+            else{
+                console.log('*** search key not full');
+                //HIDE THE DROPDOWN VALUES
+                var dropdownDiv=document.getElementById('streetDropdown');
+                dropdownDiv.classList.remove("acshow");
+                dropdownDiv.classList.add("achide");
+                //CLEAR THE DROPDOWN VALUES
+                var allValues=[];
+                component.set('v.addresses',allValues); 
+                searchInfo.timeId=undefined;
+                component.set("v.searchInfo",searchInfo);
+            }
+        }        
+    },
+    getpostal : function(component, event, helper) {
+        
+        var isblank='false';
+		var index=component.get("v.verifiedindex");
+        var city=document.getElementById('city'+index).value;
+        var state=document.getElementById('state'+index).value;
+        var street=document.getElementById('s'+index).value;
+		var country=document.getElementById('country'+index).value;     
+        var zip=document.getElementById('zip'+index).value;
+        console.log('index in get postal'+index);
+        var isverificationNeeded=document.getElementById('need'+index).checked;
+        
+        if(isverificationNeeded==false){
+            
+            var street2='';
+            if(document.getElementById('s2'+index)!=null){
+                street2 =document.getElementById('s2'+index).value;
+            }
+                      
+            if(city!='' || state!='' || street!='')
+                isblank='false';
+            else
+                isblank='true';
+            
+            var el=document.getElementById('s'+index);  
+            var _x = 0;
+            var _y = 0;
+            
+            try{
+                while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+                    _x += el.offsetLeft - el.scrollLeft;
+                    _y += el.offsetTop - el.scrollTop;
+                    el = el.offsetParent;
+                }	
+            }catch(e){}
+            
+            {            
+                var action = component.get("c.getpostalcode");
+                action.setParams({
+                    "street": street,
+                    "city": city,
+                    "state": state,
+                    "country": country,
+                    "zip": zip,
+                    "street2":street2
+                });
+                
+                action.setCallback(this, function(response) {            
+                    if (response.getState() == "SUCCESS") {            
+                        document.getElementById('zip'+index).value=response.getReturnValue(); 
+                        if(response.getReturnValue()!=''){                             
+                            component.set("v.verifiedindex",parseInt(index)+1);                                            
+                            //  NOW MAKE THE REVERIY RUN
+                            document.getElementById('verifyagain').click();
+                        }
+                        else{
+                            // OPEN THE ALERT BOX   
+                            
+                            document.getElementById("ErrorboxMain").style.display='';                        
+                            document.getElementById("Errorbox").style.paddingTop=(_y-20)+'px';   
+                            
+                            //   document.getElementById("Errorbox").style.marginTop=10+(parseInt(index)*15);
+                            
+                            if((parseInt(index)+1)%2==0){
+                                document.getElementById("Errorbox").style.cssFloat='Right';
+                                document.getElementById("Errorbox").style.marginLeft='0px';
+                                document.getElementById("Errorbox").style.marginRight='33px';
+                            }else{
+                                document.getElementById("Errorbox").style.cssFloat='Left';
+                                document.getElementById("Errorbox").style.marginLeft='33px';
+                                document.getElementById("Errorbox").style.marginRight='0px';
+                            }
+                            
+                            document.getElementById("theMessage").innerHTML=document.getElementById('s'+index).value+' '+street2+' '+document.getElementById('city'+index).value+' '+document.getElementById('state'+index).value;
+                            if(isblank=='false'){
+                                document.getElementById("ErrorboxHeader").innerHTML='YOU ENTERED AN UNKNOWN ADDRESS';
+                            }else{
+                                document.getElementById("ErrorboxHeader").innerHTML='YOU DID NOT ENTER ENOUGH INFORMATION';
+                            }                                                        
+                        }
+                    }
+                });
+                $A.enqueueAction(action);       
+            } 
+        }else{
+           
+            var verifiedIndex=component.get('v.verifiedindex');
+            verifiedIndex=parseInt(verifiedIndex)+1;
+        	component.set('v.verifiedindex',verifiedIndex);                                        
+            //  NOW MAKE THE REVERIY RUN          
+            //  document.getElementById('verifyagain').click();
+            $A.enqueueAction(component.get('c.reverify'));
+        }
+    },
+    saveAddressAfterSucess : function(component, event, helper) { 
+       
+        var addresses=component.get('v.sObjaddresses');
+        for(var i=0;i<addresses.length;i++){           
+            var oneRow=addresses[i].oneRow; 
+            for(var j=0;j<oneRow.length;j++){
+                var index=oneRow[j].Index;                
+                addresses[i].oneRow[j].cityvalue=document.getElementById('city'+index).value;
+                addresses[i].oneRow[j].statevalue=document.getElementById('state'+index).value;               
+                addresses[i].oneRow[j].streetvalue=document.getElementById('s'+index).value;             
+                addresses[i].oneRow[j].countryvalue=document.getElementById('country'+index).value;
+                addresses[i].oneRow[j].postalcodevalue=document.getElementById('zip'+index).value;
+                
+                addresses[i].oneRow[j].IsSelectedToVerify=document.getElementById('need'+index).checked;
+                
+                //IF STREET 2 IS ENABLED
+                if(addresses[i].oneRow[j].street2Enabled){
+                    addresses[i].oneRow[j].street2value=document.getElementById('s2'+index).value;
+                }
+            }
+        }       
+        
+        var theaddresses=JSON.stringify(addresses);
+        console.log(theaddresses);
+        component.set('v.AddressString',theaddresses);
+
+        
+        //VG-27/11/2025- Commenting the update address code to move this component before object creation.
+
+        const idRegex = /^[a-zA-Z0-9]{15}$|^[a-zA-Z0-9]{18}$/;
+        
+        component.set('v.readyToMove','true');
+        component.set('v.doingProcess','false');
+        
+    },
+    //HIDE THE SUGGESTIONS IF HOVER IS REMOVED 
+    hideAutoSuggestion : function(component) {
+        console.log('hideAutoSuggestion');
+        var index = component.get('v.index');
+        if(document.getElementById('s'+index)!=null){
+            console.log('hover removed');
+            var dropdownDiv=document.getElementById('streetDropdown');
+            dropdownDiv.classList.remove("acshow");
+            dropdownDiv.classList.add("achide");
+        }
+    },
+    populateAddress : function(component) {
+        
+    }
+})
