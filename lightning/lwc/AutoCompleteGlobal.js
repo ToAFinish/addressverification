b04ddcb9e7b3910089e360ca786b39f6b3be8481
Joseph Dindinger <signup@toafinish.com>
2026-01-23 16:21:27 -0600

    international support

--- lightning/lwc/AutoCompleteGlobal.js
+++ lightning/lwc/AutoCompleteGlobal.js
@@ -1,243 +1,157 @@
-import { LightningElement,wire,api,track } from 'lwc';
-import getSuggestions from '@salesforce/apex/AutoComplete.getaddresses';
-import getInternationalAdressText from '@salesforce/apex/AutoComplete.getInternationalAdressText';
-import getInternationaladdresses from '@salesforce/apex/AutoComplete.getInternationaladdresses';
-import getInternationalCompleteaddresses from '@salesforce/apex/AutoComplete.getInternationalCompleteaddresses';
-export default class AutoCompleteGlobal extends LightningElement {
-    @api searchRecords = [];
-    @api required = false;
-    @track txtclassname = 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click';
+import { LightningElement, api, track } from 'lwc';
+
+import getSuggestions from '@salesforce/apex/smartystreets.AutoComplete.getaddresses';
+import getAutocompleteEntries from '@salesforce/apex/smartystreets.AutoComplete.getAutocompleteEntries';
+import getInternationalAdressText from '@salesforce/apex/smartystreets.AutoComplete.getInternationalAdressText';
+import getInternationaladdresses from '@salesforce/apex/smartystreets.AutoComplete.getInternationaladdresses';
+import getInternationalCompleteaddresses from '@salesforce/apex/smartystreets.AutoComplete.getInternationalCompleteaddresses';
+
+export default class AutoCompleteGlobalMerge extends LightningElement {
+
     @track street;
     @track city;
     @track state;
     @track zip;
-    @track isDataAvailable = false;
-    @track autocompleteEntries = false;
-    @track search;
-    @track searchtext;
     @track country;
+
+    @track searchRecords = [];
+    @track modalSuggestions = [];
+
     @track interNationalAddresList = [];
     @track interNationalAddresSecondStepList = [];
+
     @track isInternational = false;
+    @track isDataAvailable = false;
+    @track autocompleteEntries = false;
+    @track showSpinner = false;
 
-    @track stopSubmit = false;
-    @track showSpinner = false
-    @track index;
-    @track countryName;
-    @track searchInfo = {};
-
+    search;
+    searchtext;
+    index;
+    get resolvedCountry() {
+        return (this.country && this.country.trim())
+            ? this.country.trim()
+            : 'US';
+    }
     handleCountryChange(event) {
         this.country = event.target.value;
     }
 
     searchField(event) {
+        const value = event.target.value;
+        this.street = value;
         this.isDataAvailable = false;
-         var currentText = event.target.value;
-        if(this.country && this.country!='' && this.country!=undefined && this.country!='undefined'){
-            currentText = currentText+';'+this.country;
-        }
-        else{
-            this.country = 'US';
-            currentText = currentText+';US';
-        }
-        if(this.country!='US'){
+
+        if (!value || value.length < 3) return;
+
+        const params = value + ';' + this.resolvedCountry;
+
+        if (this.resolvedCountry !== 'US') {
+            this.isInternational = true;
             this.interNationalAddresList = [];
             this.interNationalAddresSecondStepList = [];
 
-            getInternationalAdressText({ Parameters: currentText })
+            getInternationalAdressText({ Parameters: params })
                 .then(result => {
-                    console.log('******* THE API CALLED');
-
                     this.interNationalAddresList = result || [];
-                    this.isInternational = true;
-
-                    if (this.interNationalAddresList.length > 0) {
-                        this.index = event?.target?.dataset?.index;
-                        this.toggleDropdown(true);
-                        this.stopSubmit = true;
-                        this.isDataAvailable = true;
-                    } else {
-                        this.toggleDropdown(false);
-                        this.stopSubmit = false;
-                    }
+                    this.isDataAvailable = this.interNationalAddresList.length > 0;
                 })
-                .catch(error => {
-                    console.error(error);
-                });
-        }
-        else{
-           
-            this.LoadingText = true;
+                .catch(console.error);
+        } else {
             this.isInternational = false;
-            getSuggestions({Parameters:currentText})
-            .then(result => {
-                this.searchRecords= result;
-                this.LoadingText = false;
-                this.isDataAvailable = true;
-                this.txtclassname =  result.length > 0 ? 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-is-open' : 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click';
-                if(currentText.length > 0 && result.length == 0) {
-                    this.messageFlag = true;
-                }
-                else {
-                    this.messageFlag = false;
-                }
-
-                if(this.selectRecordId != null && this.selectRecordId.length > 0) {
-                    this.iconFlag = false;
-                    this.clearIconFlag = true;
-                }
-                else {
-                    this.iconFlag = true;
-                    this.clearIconFlag = false;
-                }
-            })
-            .catch(error => {
-                console.log('-------error-------------'+error);
-            });
+            this.country = this.resolvedCountry;
+            getSuggestions({ Parameters: params })
+                .then(result => {
+                    this.searchRecords = result;
+                    this.isDataAvailable = result.length > 0;
+                })
+                .catch(console.error);
         }
     }
-    
 
-    useSuggestion(event){
-        let suggestionId=event.target.id;
-        //GET EXACT INDEX FROM ID
-        if(suggestionId.includes('-')){
-            suggestionId=suggestionId.substring(0,suggestionId.indexOf('-'));
-        }
-        var singleAddress = this.searchRecords[suggestionId];
-         if(singleAddress.isSelect){
-            this.autocompleteEntries = true;
-            this.search = singleAddress.search;
-            this.searchtext = singleAddress.selected;
-            return;
-         }
-        
-        this.street = singleAddress.street_line;
-        this.city = singleAddress.city;
-        this.state = singleAddress.state;
-        this.zip = singleAddress.zipcode;
-        this.isDataAvailable = false;
-        this.suggestions=[];
-    }
-
-    getSelected(event) {
-        const singleAddress = event.detail.theAddress;
-        this.street = singleAddress.street_line;
-        this.city = singleAddress.city;
-        this.state = singleAddress.state;
-        this.zip = singleAddress.zipcode;
-        this.isDataAvailable = false;
-        this.suggestions=[];
-        this.autocompleteEntries = false;
-        this.search = undefined;
-        this.searchtext = undefined;
-    }
-    closeSelection() {
-        this.autocompleteEntries = false;
-        this.search = undefined;
-        this.searchtext = undefined;
-        this.isDataAvailable = false;
-        this.suggestions=[];
-    }
-
-    /* ==============================
-       SECOND STEP – PARTIAL ADDRESS
-       ============================== */
-    finalAddressSuggestions(event) {
-
-    const index = event.currentTarget.dataset.index;
-    const selected = this.interNationalAddresList[index];
-    if (!selected) return;
-
-    const addressID = selected.address_id;
-    const entries = selected.entries;
-
-    if (entries && entries > 1) {
-        this.showSpinner = true;
-
-        getInternationaladdresses({
-            addressId1: addressID,
-            countryName: this.country
-        })
-            .then(result => {
-                this.interNationalAddresList = [];
-                this.interNationalAddresSecondStepList = result || [];
-                this.toggleDropdown(this.interNationalAddresSecondStepList.length > 0);
-                this.stopSubmit = this.interNationalAddresSecondStepList.length > 0;
-            })
-            .catch(error => {
-                console.error(error);
-            })
-            .finally(() => {
-                this.showSpinner = false;
-            });
-
-    } else {
-        this.getAddressDetails(addressID);
-        this.isDataAvailable = false;
-    }
-}
-
-
-    /* ==============================
-       FINAL STEP – COMPLETE ADDRESS
-       ============================== */
-   finalCompleteAddressSuggestions(event) {
+    useSuggestion(event) {
         const index = event.currentTarget.dataset.index;
-        const selected = this.interNationalAddresSecondStepList[index];
-        if (selected) {
+        const selected = this.searchRecords[index];
+
+        if (selected.isSelect) {
+            this.openModal(selected.search, selected.selected);
+            return;
+        }
+
+        this.setAddress(selected);
+    }
+
+    openModal(search, selected) {
+        this.search = search;
+        this.searchtext = selected;
+        this.autocompleteEntries = true;
+
+        getAutocompleteEntries({ prefix: search, selected: selected })
+            .then(result => {
+                this.modalSuggestions = result;
+            })
+            .catch(console.error);
+    }
+
+    rowSelected(event) {
+        const index = event.currentTarget.dataset.index;
+        this.setAddress(this.modalSuggestions[index]);
+        this.closeModal();
+    }
+
+    closeModal() {
+        this.autocompleteEntries = false;
+        this.modalSuggestions = [];
+    }
+
+    finalAddressSuggestions(event) {
+        const index = event.currentTarget.dataset.index;
+        const selected = this.interNationalAddresList[index];
+
+        if (selected.entries > 1) {
+            this.showSpinner = true;
+            getInternationaladdresses({
+                addressId1: selected.address_id,
+                countryName: this.resolvedCountry
+            })
+                .then(result => {
+                    this.interNationalAddresList = [];
+                    this.interNationalAddresSecondStepList = result || [];
+                })
+                .finally(() => this.showSpinner = false);
+        } else {
             this.getAddressDetails(selected.address_id);
         }
     }
 
-
-    toggleDropdown(show) {
-        const dropdown = this.template.querySelector('.street-dropdown');
-        if (dropdown) {
-            dropdown.classList.toggle('acshow', show);
-            dropdown.classList.toggle('achide', !show);
-        }
+    finalCompleteAddressSuggestions(event) {
+        const index = event.currentTarget.dataset.index;
+        this.getAddressDetails(this.interNationalAddresSecondStepList[index].address_id);
     }
 
-getAddressDetails(addressID) {
-    const index = this.index;
-    const country = this.country;
-
-    if (!addressID) return;
-
-    this.showSpinner = true;
-
-    getInternationalCompleteaddresses({
-        addressId1: addressID,
-        countryName: country
-    })
-        .then(result => {
-            console.log('******* THE API CALLED');
-
-            if (result && result.length > 0) {
-                const singleAddress = result[0];
-                // clear second step suggestions
-                this.interNationalAddresSecondStepList = [];
-
-                /* ==============================
-                   SET INPUT VALUES (LWC WAY)
-                   ============================== */
-                this.street = singleAddress.street;
-                this.city = singleAddress.locality;
-                this.state = singleAddress.administrative_area;
-                this.zip = singleAddress.postal_code;
-
-                // hide dropdown
-                this.toggleDropdown(false);
-                this.stopSubmit = false;
+    getAddressDetails(addressID) {
+        this.showSpinner = true;
+        getInternationalCompleteaddresses({
+            addressId1: addressID,
+            countryName: this.resolvedCountry
+        })
+            .then(result => {
+                const addr = result[0];
+                this.street = addr.street;
+                this.city = addr.locality;
+                this.state = addr.administrative_area;
+                this.zip = addr.postal_code;
                 this.isDataAvailable = false;
-            }
-        })
-        .catch(error => {
-            console.error(error);
-        })
-        .finally(() => {
-            this.showSpinner = false;
-        });
-}
+            })
+            .finally(() => this.showSpinner = false);
+    }
+
+    setAddress(addr) {
+        this.street = addr.street_line;
+        this.city = addr.city;
+        this.state = addr.state;
+        this.zip = addr.zipcode;
+        this.isDataAvailable = false;
+    }
+
 }
